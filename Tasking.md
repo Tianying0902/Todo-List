9e9a0105f278db76ba850e40bdf80498784d0b04
Tianying0902 <Aimee_wang0902@163.com>
2022-11-23 10:01:47 +0800

    [Tianying]feat: add task to new conponent List

--- Tasking.md
+++ Tasking.md
@@ -1,13 +1,25 @@
 # Tasking here
 
-### Task 1
+### Task 1 create component Header
 
 - [✅] Header: content[todos]
 - [✅]Header: style[size/color]
 
-### Task 2
+### Task 2 create Component Todo
 
 - [✅]Content:arrow img
+- [✅]Content:state
 - [✅]Content:input
 - [✅]Content:style
   [flex/center/margin/padding]
+
+### Task 3 addItem
+
+- [✅]get keycode when key down
+- [✅]if content is empty show warning
+- [✅]put new task into array
+
+### Task 4 create component List
+
+- [✅]create List to contain taskList
+- [✅]use array as props
--- react-todolist/src/Todo.js
+++ react-todolist/src/Todo.js
@@ -1,8 +1,11 @@
-import "./style.css/todo.css";
+import "./style.css/Todo.css";
 import React from "react";
+import List from "./List";
+import { useState } from "react";
+
 // import ad from "./assets/ad.png";
 // import au from "./assets/au.png";
-import { useState } from "react";
+
 const Todo = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [tasks, setTasks] = useState([]);
@@ -17,19 +20,31 @@ const Todo = () => {
         setTasks([newTask, ...tasks]);
         content.value = "";
       }
+      console.log(tasks);
     }
   };
   return (
-    <div className="inputContent">
-      <img
-        // src="./assets/au.png"
-        src={isOpen ? "./assets/ad.png" : "./assets/au.png"}
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-        alt="arrow-up"
-      />
-      <input id="inputBox" placeholder="what's next" onKeyDown={addTask} />
+    <div className="bigBox">
+      <div className="inputContent">
+        <img
+          // src="./assets/au.png"
+          src={isOpen ? "./assets/ad.png" : "./assets/au.png"}
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+          alt="arrow-up"
+        />
+        <input id="inputBox" placeholder="what's next" onKeyDown={addTask} />
+      </div>
+      <List lists={tasks} />
+      {/* <div className="list">
+        {tasks.map((task, index) => (
+          <li key={index} className="task">
+            {task.text}
+            <span>X</span>
+          </li>
+        ))}
+      </div> */}
     </div>
   );
 };
