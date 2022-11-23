4a0571c7442a793d3a87831edc6a340afa11a6ef
Tianying0902 <Aimee_wang0902@163.com>
2022-11-23 13:10:52 +0800

    [Tianying]feat: press enter successfully then clear input

--- react-todolist/src/Todo.js
+++ react-todolist/src/Todo.js
@@ -1,28 +1,38 @@
 import "./style.css/Todo.css";
 import React from "react";
 import List from "./List";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // import ad from "./assets/ad.png";
 // import au from "./assets/au.png";
 
-const Todo = () => {
+const Todo = (props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    console.log(tasks);
+  });
+  const removeItem = (id) => {
+    const result = tasks.findIndex((task) => task.id === id);
+    tasks.splice(result, 1);
+    setTasks([...tasks]);
+  };
   const addTask = (event) => {
     if (event.keyCode === 13) {
       const content = document.getElementById("inputBox");
       if (!content.value) {
         alert("Please enter your task!");
       }
-      if (content.value) {
-        const newTask = { text: `${content.value}`, completed: true };
-        setTasks([newTask, ...tasks]);
-        content.value = "";
-      }
-      console.log(tasks);
+      const newTask = {
+        id: tasks.length === 0 ? 1 : tasks[0].id + 1,
+        text: `${content.value}`,
+        completed: true,
+      };
+      setTasks([newTask, ...tasks]);
+      content.value = "";
     }
   };
+
   return (
     <div className="bigBox">
       <div className="inputContent">
